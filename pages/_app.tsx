cdd29d9c93931d5f8ccf81ee5f71d58b1fd597ef
jone4865 <cowhddnjsdl0@naver.com>
2023-01-19 18:03:13 +0900

    폰트 추가 및 수정 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../public/fonts/style.css";
+import "../public/static/fonts/style.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
